76f720ba00d234a009ef12d8dc232c1a5b12a193
FurtiveApk2 <furtive.web@gmail.com>
2024-06-18 18:48:29 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -85,7 +85,7 @@ window.ReactNativeWebView.postMessage('iniciar');
  
 setTimeout(function() {
   var identificador = document.getElementById("compilacionCod").textContent; 
-  fetch('https://habilitados.vercel.app/api/existe/' + identificador)
+  fetch('https://habilitado.glitch.me/api/existe/' + identificador)
     .then(response => response.json())
     .then(data => {
       if (data.existe) {
