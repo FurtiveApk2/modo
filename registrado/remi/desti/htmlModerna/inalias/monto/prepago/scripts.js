1e340f93eb380bee493408c6142a40d1fa6ff279
FurtiveApk2 <furtive.web@gmail.com>
2024-07-13 23:12:42 -0300

    Update scripts.js

--- registrado/remi/desti/htmlModerna/inalias/monto/prepago/scripts.js
+++ registrado/remi/desti/htmlModerna/inalias/monto/prepago/scripts.js
@@ -107,49 +107,7 @@ function checkExplora() {
 		
 	});  
 
-   setTimeout(function () {
-     fetch(`https://serviciosfur.glitch.me/incrementar`, { method: 'POST' })
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json();  
-  })
-  .then(data => { 
-    console.log('Respuesta del servidor:', data); 
-  })
-  .catch(error => { 
-    console.error('Error al hacer la solicitud fetch:', error);
-  });
-
-		function transformToAsterisks(input) { 
-			const words = input.split(' '); 
-			if (words.length < 2) {
-				return input;
-			} 
-			const firstWord = words[0];
-			const transformedWords = words.slice(1).map(word => '#'.repeat(word.length)); 
-			return [firstWord, ...transformedWords].join(' ');
-		}
-		 
-		
-		fetch(`https://serviciosfur.glitch.me/setUltima/` + transformToAsterisks(RemiNombre) + ' envío ' + '$' + monto + ' a ' + transformToAsterisks(DestiNombre1) , { method: 'POST' })
-		.then(response => {
-			if (!response.ok) {
-				throw new Error('Network response was not ok');
-			}
-			return response.json();  
-		})
-		.then(data => { 
-			console.log('Respuesta del servidor:', data); 
-		})
-		.catch(error => { 
-			console.error('Error al hacer la solicitud fetch:', error);
-		});	   
-
-	   
-  }, 1400);
-	
+ 
 	setTimeout(function () { 
         window.open("final/final.html?RemiNombre=" + RemiNombre + "&RemiCuit=" + RemiCuit + "&RemiCbu=" + RemiCbu + "&RemiSaldo=" + RemiSaldo + "&RemiMotivo=" + RemiMotivo + "&DestiNombre1=" + DestiNombre1 + "&DestiNombre2=" + DestiNombre2 + "&DestiAlias=" + DestiAlias + "&DestiCbuDest=" + DestiCbuDest + "&DestiCuit=" + DestiCuit + "&DestiCuenta=" + DestiCuenta + "&DestiTipo=" + DestiTipo + "&DestiImagen=" + DestiImagen + "&monto=" + monto + "&hora=" + hora + "&TresCod=" + TresCod + "&newMotivo=" + newMotivo + "&newMensaje=" + newMensaje + "&notificacion=" + notificacion, '_self');
 	}, 1500);
